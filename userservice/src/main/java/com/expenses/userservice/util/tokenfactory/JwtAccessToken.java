e9f29b73029c943108fae4a1b44629a9e680aa67
gopichand2421 <gopichand2416@gmail.com>
2025-01-05 04:07:16 -0900

    jwt service token is removed

--- userservice/src/main/java/com/expenses/userservice/util/tokenfactory/JwtAccessToken.java
+++ userservice/src/main/java/com/expenses/userservice/util/tokenfactory/JwtAccessToken.java
@@ -1,39 +1,36 @@
-package com.expenses.userservice.services;
+package com.expenses.userservice.util.tokenfactory;
 
 import com.expenses.userservice.entities.Users;
+import com.expenses.userservice.config.ApplicationConfigProperties;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.Map;
 import java.util.Objects;
 
-@Service
-public class JwtService {
+public class JwtAccessToken implements  AccessToken{
 
-    @Value("${userservice.jwt.expiration-time}")
-    private long expirationTime;
 
-    @Value("${userservice.jwt.secret-key}")
-    private String secretKey;
-
-    public String generateToken(Map<String, Object> claims, Users user, Long expiration){
+    @Override
+    public String generateToken(Map<String, Object> claims, Users user, Long expiration, String hashingKey){
         String username = Objects.nonNull(user.getUsername()) ? user.getUsername() : user.getEmail();
         return Jwts.builder()
                 .setSubject(username)
                 .addClaims(claims)
-                .signWith(getSignInKey())
+                .signWith(getSignInKey(hashingKey))
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis()+ expiration))
                 .compact();
     }
 
-    private Key getSignInKey(){
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    private Key getSignInKey(final String hasingKey){
+        byte[] keyBytes = Decoders.BASE64.decode(hasingKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
